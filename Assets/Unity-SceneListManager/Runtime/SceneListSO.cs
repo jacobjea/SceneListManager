040e8ee24a00e1a782829dd18489fde13d0ae3db
jacobjea <jsw992@gmail.com>
2023-09-12 18:57:55 +0900

    초기 커밋

---
+++ Assets/Unity-SceneListManager/Runtime/SceneListSO.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+
+public class SceneListSO : ScriptableObject
+{
+    public List<SceneAsset> sceneList = new List<SceneAsset>();
+
+    public void Save()
+    {
+        EditorUtility.SetDirty(this);
+    }
+
+    public void UpToIndex(SceneAsset scene)
+    {
+        int targetIndex = sceneList.IndexOf(scene);
+        if (targetIndex == 0) return;
+
+        RePlaceList(targetIndex, targetIndex - 1);
+    }
+
+    public void DownToIndex(SceneAsset scene)
+    {
+        int targetIndex = sceneList.IndexOf(scene);
+        if (targetIndex == sceneList.Count-1) return;
+
+        RePlaceList(targetIndex, targetIndex + 1);
+    }
+
+    private void RePlaceList(int targetIndex, int replaceIndex)
+    {
+        SceneAsset temp = sceneList[replaceIndex];
+        sceneList[replaceIndex] = sceneList[targetIndex];
+        sceneList[targetIndex] = temp;
+
+        Save();
+    }
+}
